c194d20ea1010a24f8125ba03ad53454ef1e08cd
Hnooz <mohammededris23@gmail.com>
2021-03-03 09:16:52 +0200

    wip

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -7,12 +7,7 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __invoke()
     {
         if (request()->has('state')) 
         {
@@ -37,69 +32,4 @@ class ReportController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Report  $report
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Report $report)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Report  $report
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Report $report)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Report  $report
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Report $report)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Report  $report
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Report $report)
-    {
-        //
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', fn() => redirect()->route('dashboard.index'));
 
@@ -37,10 +27,5 @@ Route::middleware('auth')->prefix('/dashboard')->group(function () {
     Route::put('/products/{product}', 'ProductController@update')->name('products.update');
     Route::delete('/products/{product}', 'ProductController@destroy')->name('products.destroy');
 
-    Route::get('/reports', 'ReportController@index')->name('reports.index');
-    Route::get('/products/create', 'ProductController@create')->name('products.create');
-    Route::post('/products', 'ProductController@store')->name('products.store');
-    Route::get('/products/{product}/edit', 'ProductController@edit')->name('products.edit');
-    Route::put('/products/{product}', 'ProductController@update')->name('products.update');
-    Route::delete('/products/{product}', 'ProductController@destroy')->name('products.destroy');
+    Route::get('/reports', 'ReportController')->name('reports.index');
 });
